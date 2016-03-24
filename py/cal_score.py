fece1713e2bc4690b94c5977057ca96ae9737502
bambooom <nmz89825@gmail.com>
2016-03-24 17:59:40 +0800

    cal C pts

--- py/cal_score.py
+++ py/cal_score.py
@@ -27,3 +27,31 @@ def cal_A_pts(energy=0,sat_fat=0,sugar=0,sodium=0):
             A_pts[i] = 10
 
     return sum(A_pts)
+
+def cal_C_pts(fvn=0,fibre=0,protein=0):
+    ```
+    fruit, vegetable & nuts %
+    fibre in unit g
+    protetin in unit g
+    ```
+    C_pts = [0,0,0]
+    if fvn <= 40:
+        C_pts[0] = 0
+    elif fvn <= 60:
+        C_pts[0] = 1
+    elif fvn <= 80:
+        C_pts[0] = 2
+    else: # no 3, 4 pts
+        C_pts[0] = 5
+
+    C_nutrition = [fibre,protein]
+    C_coeff = [0.7, 0.9]
+    for i in range(len(C_nutrition)):
+        if C_nutrition[i] <= 5*C_coeff[i]:
+            if C_nutrition[i] == math.floor(C_nutrition[i]):
+                C_pts[i+1] = C_nutrition[i] - 1
+            else:
+                C_pts[i+1] = math.floor(C_nutrition[i]/C_coeff[i])
+        else:
+            A_pts[i] = 5
+    return sum(C_pts)
